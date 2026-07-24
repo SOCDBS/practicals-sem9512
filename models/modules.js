c38cc4e461e2ebf68dfe66bca9583460b8b8a142
sem9512 <semjael4@gmail.com>
2026-07-24 12:36:51 +0800

    completed to practical orm4

--- models/modules.js
+++ models/modules.js
@@ -1,72 +1,71 @@
-const { query } = require("../database");
-const {
-  EMPTY_RESULT_ERROR,
-  SQL_ERROR_CODE,
-  UNIQUE_VIOLATION_ERROR,
-} = require("../errors");
+const { PrismaClient, Prisma } = require('@prisma/client');
+const prisma = new PrismaClient();
 
 module.exports.create = function create(code, name, credit) {
-  return query("CALL create_module($1, $2, $3)", [code, name, credit])
-    .then(function (result) {
-      console.log("Module created successfully");
-    })
-    .catch(function (error) {
-      throw error;
-    });
-};
-
-module.exports.retrieveByCode = function retrieveByCode(code) {
-  const sql = `SELECT * FROM module WHERE mod_code = $1`;
-  return query(sql, [code]).then(function (result) {
-    const rows = result.rows;
-
-    if (rows.length === 0) {
-      // Note: result.rowCount returns the number of rows processed instead of returned
-      // Read more: https://node-postgres.com/apis/result#resultrowcount-int--null
-      throw new EMPTY_RESULT_ERROR(`Module ${code} not found!`);
-    }
-
-    return rows[0];
-  });
-};
-
-module.exports.deleteByCode = function deleteByCode(code) {
-  return query("CALL delete_module($1)", [code])
-    .then(function (result) {
-      console.log("Module deleted successfully");
-    })
-    .catch(function (error) {
-      throw error;
+    return prisma.module.create({
+        data: {
+            modCode: code,
+            modName: name,
+            creditUnit: parseInt(credit, 10)
+        }
+    }).then(function (module) {
+        return module;
+    }).catch(function (error) {
+        if (error instanceof Prisma.PrismaClientKnownRequestError && error.code === 'P2002') {
+            throw new Error(`The module ${code} already exists`);
+        }
+        throw error;
     });
 };
 
 module.exports.updateByCode = function updateByCode(code, credit) {
-  return query("CALL update_module($1, $2)", [code, credit])
-    .then(function (result) {
-      console.log("Module updated successfully");
-    })
-    .catch(function (error) {
-      throw error;
+    return prisma.module.update({
+        where: {
+            modCode: code
+        },
+        data: {
+            creditUnit: parseInt(credit, 10)
+        }
+    }).then(function (module) {
+        return module;
+    }).catch(function (error) {
+        if (error instanceof Prisma.PrismaClientKnownRequestError && error.code === 'P2025') {
+            throw new Error(`The module ${code} is not found`);
+        }
+        throw error;
+    });
+};
+
+module.exports.deleteByCode = function deleteByCode(code) {
+    return prisma.module.delete({
+        where: {
+            modCode: code
+        }
+    }).then(function (module) {
+        return module;
+    }).catch(function (error) {
+        if (error instanceof Prisma.PrismaClientKnownRequestError && error.code === 'P2025') {
+            throw new Error(`The module ${code} is not found`);
+        }
+        throw error;
     });
 };
 
 module.exports.retrieveAll = function retrieveAll() {
-  const sql = `SELECT * FROM module`;
-  return query(sql).then(function (result) {
-    return result.rows;
-  });
+    return prisma.module.findMany();
 };
 
-module.exports.retrieveBulk = function retrieveBulk(codes) {
-  const sql = "SELECT * FROM module WHERE code IN ($1)";
-  return query(sql, [codes]).then(function (response) {
-    const rows = response.rows;
-    const result = {};
-    for (let i = 0; i < rows.length; i += 1) {
-      const row = rows[i];
-      const code = row.code;
-      result[code] = row;
-    }
-    return result;
-  });
+module.exports.retrieveByCode = function retrieveByCode(code) {
+    return prisma.module.findUnique({
+        where: {
+            modCode: code
+        },
+    }).then(function (module) {
+        if (!module) {
+            throw new Error(`The module ${code} is not found`);
+        }
+        return module;
+    }).catch(function (error) {
+        throw error;
+    });
 };
